2ab7f5c5ed5458019c1d36e7bdcaaf012686293b
Robert Moser <rmoser@alum.mit.edu>
2020-04-11 13:26:28 -0400

    ps2 files.
    SeekerRobot working.

--- 6002x/ps2/ps2.py
+++ 6002x/ps2/ps2.py
@@ -9,6 +9,8 @@ import pylab
 import os
 os.environ["OPENBLAS_NUM_THREADS"] = "1"
 import numpy as np
+import time
+random.seed(0)
 
 ##################
 ## Comment/uncomment the relevant lines, depending on which version of Python you have
@@ -34,7 +36,10 @@ class Position(object):
         """
         self.x = x
         self.y = y
-        
+
+    def __repr__(self):
+        return str((self.x, self.y))
+
     def getX(self):
         return self.x
     
@@ -90,9 +95,20 @@ class RectangularRoom(object):
         self.height = int(height)
         # Bool value indicates Clean (True) or Dirty (False)
         self.tiles = np.zeros(shape=(height, width), dtype=np.bool)
+        self.xcoords = np.zeros(shape=self.tiles.shape, dtype=np.int)
+        self.ycoords = np.zeros(shape=self.tiles.shape, dtype=np.int)
+        for x in range(width):
+            for y in range(height):
+                # [y,x] because numpy array indices are row, column
+                self.xcoords[y,x] = x
+                self.ycoords[y,x] = y
+        self.robots = []
 
     def __repr__(self):
-        return self.tiles.__repr__()
+        _tiles = self.tiles.astype(np.str)
+        for robot in self.robots:
+            _tiles[math.floor(robot.pos.y), math.floor(robot.pos.x)] = str(robot.direction)
+        return str(_tiles[::-1, :])  # Print in standard coords, with y increasing to the north
 
     def cleanTileAtPosition(self, pos):
         """
@@ -115,7 +131,19 @@ class RectangularRoom(object):
         returns: True if (m, n) is cleaned, False otherwise
         """
         return self.tiles[n, m]
-    
+
+    def isTilePosCleaned(self, pos):
+        """
+        Return True if the tile (m, n) has been cleaned.
+
+        Assumes that (m, n) represents a valid tile inside the room.
+
+        m: an integer
+        n: an integer
+        returns: True if (m, n) is cleaned, False otherwise
+        """
+        return self.tiles[math.floor(pos.y), math.floor(pos.x)]
+
     def getNumTiles(self):
         """
         Return the total number of tiles in the room.
@@ -138,7 +166,7 @@ class RectangularRoom(object):
 
         returns: a Position object.
         """
-        return Position(random.randint(0, self.width-1), random.randint(0, self.height-1))
+        return Position(random.random() * self.width, random.random() * self.height)
 
     def isPositionInRoom(self, pos):
         """
@@ -171,11 +199,15 @@ class Robot(object):
         speed: a float (speed > 0)
         """
         self.room = room
+        room.robots.append(self)
         self.speed = speed
+#        self.direction = self.setRobotRandomDirection()
         self.direction = random.randint(0, 359)
         self.pos = room.getRandomPosition()
         room.cleanTileAtPosition(self.pos)
 
+    def __repr__(self):
+        return f"Robot pos: {self.pos}, dir: {self.direction}, speed: {self.speed}"
 
     def getRobotPosition(self):
         """
@@ -319,6 +351,100 @@ class RandomWalkRobot(Robot):
 
 # print(runSimulation(1, 1.0, 20, 20, 0.5, 1, RandomWalkRobot, True))
 
+class OrthogonalRobot(Robot):
+    """
+    A RandomWalkRobot is a robot with the "random walk" movement strategy: it
+    chooses a new direction at random at the end of each time-step.
+    """
+    def setRobotRandomDirection(self):
+        """
+        Set a random direction for the robot
+        """
+        self.direction = random.randint(0, 3) * 90
+
+    def updatePositionAndClean(self):
+        """
+        Simulate the passage of a single time-step.
+
+        Move the robot to a new position and mark the tile it is on as having
+        been cleaned.
+        """
+        nextPos = self.getRobotPosition().getNewPosition(self.getRobotDirection(), self.speed)
+        if self.room.isPositionInRoom(nextPos):
+            self.setRobotPosition(nextPos)
+            self.room.cleanTileAtPosition(nextPos)
+        self.setRobotRandomDirection()
+
+class SeekerRobot(Robot):
+    """
+    A RandomWalkRobot is a robot with the "random walk" movement strategy: it
+    chooses a new direction at random at the end of each time-step.
+    """
+    def setRobotRandomDirection(self):
+        """
+        Set a random direction for the robot
+        """
+        self.direction = random.randint(0, 3) * 90
+
+    def nearest_dirt_direction(self):
+        # Calculate Manhattan distances to each dirty tile
+        distances = np.logical_not(self.room.tiles) * (np.abs(np.floor(self.pos.x) - self.room.xcoords) + np.abs(np.floor(self.pos.y) - self.room.ycoords))
+        y, x = np.where(distances == np.min(distances[np.nonzero(np.logical_not(self.room.tiles))]))
+        # print(x, y)
+        x = x[0]
+        y = y[0]
+        d = math.degrees(math.atan2(y - math.floor(self.pos.y), x - math.floor(self.pos.x)))
+        # Transform from standard coords to NSEW coords
+        direction = round((90 - d) % 360 / 90) * 90
+        #print(f"\nRoom:\n{self.room}")
+        #print(f"\ndistances:\n{distances}")
+        #print(f"pos: {self.pos}  facing: {self.direction}  Nearest dirt: {(x,y)}  dir: {d}->{direction}")
+        #input("Press Enter")
+        return direction
+
+    def updatePositionAndClean(self):
+        """
+        Simulate the passage of a single time-step.
+
+        Move the robot to a new position and mark the tile it is on as having
+        been cleaned.
+        """
+        # Patch for init to non-orthogonal directions
+        self.setRobotDirection(self.direction // 90 * 90)
+        #print(f"\npos: {(self.pos.x, self.pos.y)}  dir: {self.direction}")
+        # Change direction if the next tile is a clean tile or a wall
+        nextPos = self.getRobotPosition().getNewPosition(self.getRobotDirection(), self.speed)
+        found_dirt = False
+        if not self.room.isPositionInRoom(nextPos) or self.room.isTilePosCleaned(nextPos):
+            self.setRobotDirection(self.nearest_dirt_direction())
+            if False:
+                for direction in range(0, 360, 90):
+                    nextPos = self.getRobotPosition().getNewPosition(direction, self.speed)
+                    if self.room.isPositionInRoom(nextPos) and not self.room.isTilePosCleaned(nextPos):
+                        self.setRobotDirection(direction)
+                        found_dirt = True
+                if not found_dirt:
+                    # Pick a NEW direction
+                    directions = [0, 90, 180, 270]
+                    directions.remove(self.direction)
+                    for d in directions:
+                        nextPos = self.getRobotPosition().getNewPosition(d, self.speed)
+                        if not self.room.isPositionInRoom(nextPos):
+                            directions.remove(d)
+
+                    self.setRobotDirection(random.choice(directions))
+
+        #print(self.pos, self.direction)
+        #input("Press Enter...")
+        nextPos = self.getRobotPosition().getNewPosition(self.getRobotDirection(), self.speed)
+        self.setRobotPosition(nextPos)
+        self.room.cleanTileAtPosition(nextPos)
+
+
+#print(runSimulation(1, 1.0, 20, 20, 0.5, 1, OrthogonalRobot, True))
+
+print(runSimulation(2, 1.0, 50, 50, 1, 1, SeekerRobot, True))
+
 def showPlot1(title, x_label, y_label):
     """
     What information does the plot produced by this function tell you?
@@ -326,14 +452,17 @@ def showPlot1(title, x_label, y_label):
     num_robot_range = range(1, 11)
     times1 = []
     times2 = []
+    times3 = []
     for num_robots in num_robot_range:
         print("Plotting", num_robots, "robots...")
         times1.append(runSimulation(num_robots, 1.0, 20, 20, 0.8, 20, StandardRobot))
         times2.append(runSimulation(num_robots, 1.0, 20, 20, 0.8, 20, RandomWalkRobot))
+        times3.append(runSimulation(num_robots, 1.0, 20, 20, 0.8, 20, OrthogonalRobot))
     pylab.plot(num_robot_range, times1)
     pylab.plot(num_robot_range, times2)
+    pylab.plot(num_robot_range, times3)
     pylab.title(title)
-    pylab.legend(('StandardRobot', 'RandomWalkRobot'))
+    pylab.legend(('StandardRobot', 'RandomWalkRobot', 'OrthogonalRobot'))
     pylab.xlabel(x_label)
     pylab.ylabel(y_label)
     pylab.show()
@@ -370,8 +499,7 @@ def showPlot2(title, x_label, y_label):
 # 1) Write a function call to showPlot1 that generates an appropriately-labeled
 #     plot.
 #
-#       (... your call here ...)
-#
+#showPlot1("Cleaning Time vs Number of Robots", "# Robots", "Time")
 
 #
 # 2) Write a function call to showPlot2 that generates an appropriately-labeled
