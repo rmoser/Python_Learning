6462bb639912071184bb733238cfadfa79cb4302
Robert Moser <rmoser@alum.mit.edu>
2020-07-11 11:20:51 -0400

    im_proc updates
    pic_km: Kmeans function.  Adds ratio argument to accelerate kmeans by reducing pixel count
    
    pic_toon: Unfinished toonify function
    
    dist: Updates to support images, in addition to numpy arrays.
    
    _calc_grid: From wafer map.  Calculates proper x,y dimensions for a grid of images, given desired aspect ratio for the resulting array of images.

--- _Ex_ImageProc/im_proc.py
+++ _Ex_ImageProc/im_proc.py
@@ -1,5 +1,6 @@
 import PIL.Image
 import PIL.ImageWin
+import PIL.ImageFilter
 
 import _Ex_kmeans.km as km
 import numpy as np
@@ -31,72 +32,189 @@ def show_pic(image, colormap=0):
     img.show()
 
 
-def pic_km(image, k=None):
-    if  k is None:
+def pic_km(image, k=None, ratio=None):
+    if not isinstance(image, PIL.Image.Image):
+        raise TypeError(f"Requires PIL Image: {image}")
+
+    if k is None:
         k = 10
 
-    if isinstance(image, PIL.Image.Image):
-        arr = np.asarray(image)
-    else:
-        arr = image
+    # 0 < ratio <= 1:  Reduces pixels to accelerate the Kmeans execution
+    if ratio is None or ratio <= 0:
+        ratio = 1
 
-    flat = arr.reshape(-1, 4)  # Other color formats?
-    clusters, score = sp.cluster.vq.kmeans(flat.astype(float), k)  # Run Kmeans
-    assigned, score2 = sp.cluster.vq.vq(flat, clusters)  # Assign cluster id to each pixel
+    if ratio > 1:
+        ratio = 1 / ratio
+
+    large = np.asarray(image)
+    small = np.asarray(image.resize(size=np.ceil((image.width * ratio, image.height * ratio)).astype(np.int)))
+
+    flat_large = large.reshape(-1, large.shape[-1])  # Flatten to one row per pixel
+    flat_small = small.reshape(-1, large.shape[-1])
+
+    clusters, score = sp.cluster.vq.kmeans(flat_small.astype(float), k)  # Run Kmeans
+
+    assigned, score2 = sp.cluster.vq.vq(flat_large, clusters)  # Assign cluster id to each pixel in full image
     new = clusters.round(0).astype(np.uint8)[assigned]  # Generate pixel-clustered image flat array
-    new = new.reshape(arr.shape)  # Reshape to original image x/y dimensions
+    new = new.reshape(large.shape)  # Reshape to original image x/y dimensions
 
     if isinstance(image, PIL.Image.Image):
         return PIL.Image.fromarray(new)
     return new
 
 
+def pic_toon(image):
+    pic_med = image.filter(PIL.ImageFilter.MedianFilter(7))
+    pic_edge0 = pic_med.filter(PIL.ImageFilter.FIND_EDGES)
+    pic_edge1 = pic_edge0.filter(PIL.ImageFilter.MaxFilter(3))
+
+
 def dist(a, b):
-    return sp.spatial.distance.euclidean(a, b)
+    """
+        Returns distance between two images.  Squared sum of differences in pixel values
+        Image sizes must match in size (x, y), color encoding (rgb), and bits per pixel
+    """
+
+    if not isinstance(a, np.ndarray):
+        a = np.asarray(a)
+
+    if not isinstance(b, np.ndarray):
+        b = np.asarray(b)
+
+    return sp.spatial.distance.euclidean(a.flatten(), b.flatten())
 
 
 def name():
     return sys._getframe().f_code.co_name
 
 
-def grid(images, shape):
-    if np.iterable(images):
-        indices = range(np.prod(shape[:2]))
+def _calc_grid(n=None, aspect_ratio=None, image_aspect_ratio=None):
+    if not aspect_ratio:
+        aspect_ratio = 16/9.
+    elif np.iterable(aspect_ratio):
+        if len(aspect_ratio) >= 2:
+            aspect_ratio = aspect_ratio[0] / aspect_ratio[1]
+        else:
+            aspect_ratio = aspect_ratio[0]
 
-    elif isinstance(images, PIL.Image.Image):
+    if not image_aspect_ratio:
+        img_aspect_ratio = 1
+    elif np.iterable(image_aspect_ratio):
+        if len(image_aspect_ratio) >= 2:
+            img_aspect_ratio = image_aspect_ratio[0] / image_aspect_ratio[1]
+        else:
+            img_aspect_ratio = image_aspect_ratio[0]
+
+    if not np.issubdtype(type(n), np.integer):
+        if np.iterable(n):
+            n = len(n)
+        elif np.issubdtype(type(n), np.float):
+            n = np.uint(np.abs(n))
+        else:
+            n = 1
+
+    # Defines desired aspect ratio for grid
+    _x = aspect_ratio / image_aspect_ratio
+    _y = 1
+
+    v = (n / _x * _y) ** 0.5
+    y = int(round(v, 0))
+    x = int(n / y)
+    while n > y * x:
+        x += 1
+    # print(f"n: {n}  v: {v}  xy: {x} {y}")
+    return x, y
+
+
+def grid(images, shape=(), repeat=False, scale=None):
+    if not np.iterable(images):
         images = [images]
-        indices = np.zeros(shape, dtype=np.uint8)
-    else:
-        raise TypeError(f"ERROR in grid():  images arg should be list of PIL Images: {images}")
+
+    for i, img in enumerate(images):
+        if np.issubdtype(type(img), np.str_) and os.path.exists(img):
+            images[i] = PIL.Image.open(img)
+
+    repeat = bool(repeat)
+
+    if scale is None or scale <= 0:
+        scale = 1
+
+    # Image dimensions
+    img_width = np.max([x.width for x in images]) * scale
+    img_height = np.max([x.height for x in images]) * scale
+    mode = images[0].mode
+
+    if not shape:
+        shape = _calc_grid(images, aspect_ratio=None, image_aspect_ratio=img_width / img_height)
+
+    if np.issubdtype(type(shape), np.integer):
+        shape = (shape, 1)
+
+    # Grid dimensions
+    grid_width, grid_height = shape
+
+    indices = np.arange(np.prod(shape[:2])) % len(images)
+
+    image = PIL.Image.new(mode, size=(img_width * grid_width, img_height * grid_height))
+
+    # Convert all images to same color mode
+    for i in images:
+        if i.mode != mode:
+            images[i] = i.convert(mode)
+
+    for i in range(len(indices)):
+        index = i % len(images)
+        x = i % grid_width
+        y = i // grid_width
+
+        img = images[index]
+        if scale != 1:
+            img = img.resize(size=(img.width * scale, img.height * scale))
+
+        image.paste(img, (x * img_width, y * img_height))
+
+        if i == len(images) - 1 and not repeat:
+            break
+
+    return image
 
 
 if __name__ == '__main__':
+    file = 'Alpenrose Team Picture.jpeg'
     folder = r'C:\Users\rmose\Desktop\pics'
-    file = path.join(folder, 'IMG_0114.PNG')
-    folder = os.path.join(folder, "Gavin")
+    file = path.join(folder, file)
+    base, ext = os.path.splitext(file)
+    folder = os.path.join(folder, "Alpenrose")
     if not os.path.isdir(folder):
         os.mkdir(folder)
 
     pic = read_pic(file)
+    # Output as jpg
+    pic = pic.convert('RGB')
+    ext = '.jpg'
     # print(pic.shape)
     # pic.show()
 
-    dims = tuple(x//2 for x in pic.size)
+    dims = tuple(int(x) for x in pic.size)
 
-    small = pic.resize(dims)
-    small.save(os.path.join(folder, "Gavin_99.png"))
+    #small = pic.resize(dims)
+    small = pic
+    outfull = os.path.join(folder, f"Gavin___{ext}")
+    if not os.path.exists(outfull):
+        small.save(outfull)
 
-    image = PIL.Image.new('RGB', (small.width*5, small.height*4))
-    for y in range(4):
-        for x in range(5):
-            i = x + y * 5
-            print(f"Processing Image {i}...")
-            if i == 0:
-                image.paste(small, (0, 0))
-            else:
-                img = pic_km(small, 21 - i)  # .resize(small.size)
-                img.save(os.path.join(folder, f"Gavin_{str(21-i).zfill(2)}.png"))
-                image.paste(img, (x * small.width, y * small.height))
+    ratio = np.max((0.1, np.min((1., 10000 / np.prod(pic.size)))))
+    print(f"Ratio: {ratio}")
 
-    image.save(path.join(folder, 'Gavin Baseball Kmeans.PNG'))
+    for i in range(2, 20):
+        outfile = f"Gavin_{str(i).zfill(2)}{ext}"
+        outfull = os.path.join(folder, outfile)
+        if os.path.exists(outfull):
+            print(f"Already have Image {i}: {outfile}...")
+            continue
+
+        print(f"Processing Image {i}: {outfile}...")
+        img = pic_km(small, i, ratio=0.2)  # .resize(small.size)
+        img.save(os.path.join(folder, outfull))
+        continue
 
