6efe95561636fdf7689a8112599c8685050f90c9
Robert Moser <rmoser@alum.mit.edu>
2020-07-11 13:20:12 -0400

    mosaic uses many images to create mosaic of target image

---
+++ _Ex_ImageProc/mosaic.py
@@ -0,0 +1,63 @@
+import _Ex_ImageProc.im_proc as im_proc
+import PIL
+import PIL.Image
+import PIL.ImageFile
+import os
+import glob
+import numpy as np
+
+
+def mosaic(img, images):
+    (x_size, y_size) = img.size
+
+    x_small = x_size // 12
+    y_small = y_size // 8
+
+    n = len(images)
+    _images = [_img.resize(size=(x_small, y_small)) for _img in images]
+
+    image_mosaic = np.empty(shape=(x_small * 12, y_small * 8, 3))
+
+    for x in range(12):
+        for y in range(8):
+            small = np.array(img.crop((x * x_small, y * y_small, (x+1)*x_small-1, (y+1)*y_small-1)))
+            nearest = im_proc.nearest(small, _images)
+            image_mosaic[x * x_small:(x+1)*x_small-1, y * y_small:(y+1)*y_small-1] = nearest
+
+    return PIL.Image.fromarray(image_mosaic)
+
+
+def images_resize(size, in_folder, out_folder):
+    for filepath in glob.glob(os.path.join(in_folder, "*.jp*")):
+        path, file = os.path.split(filepath)
+        base, ext = os.path.splitext(file)
+        outfull = base + "_small" + ext
+        print(base, ext, outfull)
+#        continue
+
+        img = None
+        try:
+            img = im_proc.read_pic(filepath)
+        except TypeError:
+            raise
+
+        if isinstance(img, PIL.Image.Image):
+            x, y = img.size
+            if x == y:
+                crop = img  # Already square
+            elif x > y:
+                left = (x-y) // 2
+                crop = img.crop((left, 0, y, left + y))  # Center crop to square
+            else:
+                top = (y-x) // 2
+                crop = img.crop((0, top, x, top + x))  # Center crop to square
+
+            small = crop.resize(size)
+
+            small.save(os.path.join(out_folder, outfull))
+
+
+def get_images(folder):
+    return [im_proc.read_pic(file) for file in glob.glob(os.path.join(folder, "*.jp*"))]
+
+
