f3628dac187d5f97b532fc652347cdb7a0d959c5
Robert Moser <robert.j.moser@intel.com>
2021-12-21 11:35:30 -0500

    Update template

--- AdventOfCode/2015/aoc15_00.py
+++ AdventOfCode/2015/aoc15_00.py
@@ -5,13 +5,18 @@ day = 0
 import numpy as np
 import aocd
 
-text0 = ""
+text0 = """ 
+"""
 text1 = aocd.get_data(day=day, year=year)
 
 
 if __name__ == '__main__':
+    pone = ''
+    ptwo = ''
+
     text = text0
+    text = text.strip().splitlines()
 
-    print(f"AOC {year} day {day}  Part One: {0}")
+    print(f"AOC {year} day {day}  Part One: {pone}")
 
-    print(f"AOC {year} day {day}  Part Two: {1}")
+    print(f"AOC {year} day {day}  Part Two: {ptwo}")
