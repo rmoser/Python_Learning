e74ae5609e84a5d5a95909305d1bb3afbb276cb2
Robert Moser <robert.j.moser@intel.com>
2022-01-07 08:10:44 -0500

    AOC 16 correct template

--- AdventOfCode/2016/aoc16_00.py
+++ AdventOfCode/2016/aoc16_00.py
@@ -6,9 +6,17 @@ import numpy as np
 import aocd
 
 text0 = """
+Disc #1 has 5 positions; at time=0, it is at position 4.
+Disc #2 has 2 positions; at time=0, it is at position 1.
 """
 text1 = aocd.get_data(day=day, year=year)
 
+discs = dict()
+
+# Dirty Enums
+POS = 0
+START = 1
+PERIOD = 2
 
 if __name__ == '__main__':
     pone = ''
@@ -16,6 +24,13 @@ if __name__ == '__main__':
 
     text = text0
     text = text.strip().splitlines()
+    for line in text:
+        line = line.split()
+        n = int(line[1][1:])
+        period = int(line[3])
+        start = int(line[-1][:-1])
+        discs[n] = [start, n, start, period]
+
 
     print(f"AOC {year} day {day}  Part One: {pone}")
 
