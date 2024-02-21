9787520163506d1e56c6fe161c2e0bd8c6e31c0e
Robert Moser <rmoser@alum.mit.edu>
2024-02-21 13:22:48 -0500

    committing current state

---
+++ VSCode Tutorial/intro.py
@@ -0,0 +1,10 @@
+import sys
+import numpy as np
+import pandas as pd
+
+if __name__ == "__main__":
+    print("")
+    print(f"Python version: {sys.version}")
+    print(f"Numpy: {np.__version__}")
+    print(f"Pandas: {pd.__version__}")
+    print("")
