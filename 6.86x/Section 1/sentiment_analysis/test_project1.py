afb44d1801539ac0c16aeea07a8c935afead859f
rmoser2000 <rmoser@alum.mit.edu>
2025-02-13 09:20:46 -0500

    Rewrote tests for pytest framework

--- 6.86x/Section 1/sentiment_analysis/test_project1.py
+++ 6.86x/Section 1/sentiment_analysis/test_project1.py
@@ -2,8 +2,12 @@ import os
 import sys
 import time
 import traceback
+
+from winnt import TRANSACTIONMANAGER_QUERY_INFORMATION
+
 import project1 as p1
 import numpy as np
+import pytest
 
 verbose = False
 
@@ -29,307 +33,333 @@ def check_real(ex_name, f, exp_res, *args):
         res = f(*args)
     except NotImplementedError:
         log(red("FAIL"), ex_name, ": not implemented")
-        return True
+        return False
     if not np.isreal(res):
         log(red("FAIL"), ex_name, ": does not return a real number, type: ", type(res))
-        return True
-    if res != exp_res:
+        return False
+    if res != pytest.approx(exp_res):
         log(red("FAIL"), ex_name, ": incorrect answer. Expected", exp_res, ", got: ", res)
-        return True
+        return False
+    return True
 
 
 def equals(x, y):
-    if type(y) == np.ndarray:
-        return (x == y).all()
-    return x == y
+    if type(y) == np.ndarray or type(x) == np.ndarray:
+        return (x == pytest.approx(y)).all()
+    return x == pytest.approx(y)
 
 def check_tuple(ex_name, f, exp_res, *args, **kwargs):
     try:
         res = f(*args, **kwargs)
     except NotImplementedError:
         log(red("FAIL"), ex_name, ": not implemented")
-        return True
+        return False
     if not type(res) == tuple:
         log(red("FAIL"), ex_name, ": does not return a tuple, type: ", type(res))
-        return True
+        return False
     if not len(res) == len(exp_res):
         log(red("FAIL"), ex_name, ": expected a tuple of size ", len(exp_res), " but got tuple of size", len(res))
-        return True
+        return False
     if not all(equals(x, y) for x, y in zip(res, exp_res)):
         log(red("FAIL"), ex_name, ": incorrect answer. Expected", exp_res, ", got: ", res)
-        return True
+        return False
+    return True
 
 def check_array(ex_name, f, exp_res, *args):
     try:
         res = f(*args)
     except NotImplementedError:
         log(red("FAIL"), ex_name, ": not implemented")
-        return True
+        return False
     if not type(res) == np.ndarray:
         log(red("FAIL"), ex_name, ": does not return a numpy array, type: ", type(res))
-        return True
+        return False
     if not len(res) == len(exp_res):
         log(red("FAIL"), ex_name, ": expected an array of shape ", exp_res.shape, " but got array of shape", res.shape)
-        return True
+        return False
     if not all(equals(x, y) for x, y in zip(res, exp_res)):
         log(red("FAIL"), ex_name, ": incorrect answer. Expected", exp_res, ", got: ", res)
-        return True
+        return False
+    return True
 
 def check_list(ex_name, f, exp_res, *args):
     try:
         res = f(*args)
     except NotImplementedError:
         log(red("FAIL"), ex_name, ": not implemented")
-        return True
+        return False
     if not type(res) == list:
         log(red("FAIL"), ex_name, ": does not return a list, type: ", type(res))
-        return True
+        return False
     if not len(res) == len(exp_res):
         log(red("FAIL"), ex_name, ": expected a list of size ", len(exp_res), " but got list of size", len(res))
-        return True
+        return False
     if not all(equals(x, y) for x, y in zip(res, exp_res)):
         log(red("FAIL"), ex_name, ": incorrect answer. Expected", exp_res, ", got: ", res)
-        return True
+        return False
+    return True
 
 
-def check_get_order():
-    ex_name = "Get order"
-    if check_list(
+def test_get_order_00():
+    ex_name = "Get order 00"
+    if not check_list(
             ex_name, p1.get_order,
             [0], 1):
         log("You should revert `get_order` to its original implementation for this test to pass")
-        return
-    if check_list(
+        return False
+    if not check_list(
             ex_name, p1.get_order,
             [1, 0], 2):
         log("You should revert `get_order` to its original implementation for this test to pass")
-        return
+        return False
     log(green("PASS"), ex_name, "")
+    return True
 
 
-def check_hinge_loss_single():
-    ex_name = "Hinge loss single"
+def test_hinge_loss_single_00():
+    ex_name = "Hinge loss single 00"
 
     feature_vector = np.array([1, 2])
     label, theta, theta_0 = 1, np.array([-1, 1]), -0.2
     exp_res = 1 - 0.8
-    if check_real(
+    if not check_real(
             ex_name, p1.hinge_loss_single,
             exp_res, feature_vector, label, theta, theta_0):
-        return
+        return False
     log(green("PASS"), ex_name, "")
+    return True
 
 
-def check_hinge_loss_full():
-    ex_name = "Hinge loss full"
+def test_hinge_loss_single_01():
+    ex_name = "Hinge loss single 01"
+
+    feature_vector = np.array([0.13018489, 0.45342815, 0.47727826, 0.47838207, 0.24782531, 0.95400403, 0.60135646, 0.21863698 ,0.50490517, 0.22274132])
+    label = -1
+    theta = np.array([0.38406916, 0.11027105, 0.10476069, 0.10451897, 0.20175502, 0.05241068, 0.08314536, 0.22868958, 0.0990285,  0.22447564])
+    theta_0 = 0.5
+    exp_res = 2.
+    if not check_real(
+            ex_name, p1.hinge_loss_single,
+            exp_res, feature_vector, label, theta, theta_0):
+        return False
+    log(green("PASS"), ex_name, "")
+    return True
+
+
+def test_hinge_loss_full_00():
+    ex_name = "Hinge loss full 00"
 
     feature_vector = np.array([[1, 2], [1, 2]])
     label, theta, theta_0 = np.array([1, 1]), np.array([-1, 1]), -0.2
     exp_res = 1 - 0.8
-    if check_real(
+    if not check_real(
             ex_name, p1.hinge_loss_full,
             exp_res, feature_vector, label, theta, theta_0):
-        return
+        return False
 
     log(green("PASS"), ex_name, "")
+    return True
 
 
-def check_perceptron_single_update():
-    ex_name = "Perceptron single update"
+def test_perceptron_single_update_00():
+    ex_name = "Perceptron single update 00"
 
     feature_vector = np.array([1, 2])
     label, theta, theta_0 = 1, np.array([-1, 1]), -1.5
     exp_res = (np.array([0, 3]), -0.5)
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.perceptron_single_step_update,
             exp_res, feature_vector, label, theta, theta_0):
-        return
+        return False
 
     feature_vector = np.array([1, 2])
     label, theta, theta_0 = 1, np.array([-1, 1]), -1
     exp_res = (np.array([0, 3]), 0)
-    if check_tuple(
+    if not check_tuple(
             ex_name + " (boundary case)", p1.perceptron_single_step_update,
             exp_res, feature_vector, label, theta, theta_0):
-        return
+        return False
 
     log(green("PASS"), ex_name, "")
+    return True
 
 
-def check_perceptron():
-    ex_name = "Perceptron"
+def test_perceptron_00():
+    ex_name = "Perceptron 00"
 
     feature_matrix = np.array([[1, 2]])
     labels = np.array([1])
     T = 1
     exp_res = (np.array([1, 2]), 1)
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.perceptron,
             exp_res, feature_matrix, labels, T):
-        return
+        return False
 
     feature_matrix = np.array([[1, 2], [-1, 0]])
     labels = np.array([1, 1])
     T = 1
     exp_res = (np.array([0, 2]), 2)
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.perceptron,
             exp_res, feature_matrix, labels, T):
-        return
+        return False
 
     feature_matrix = np.array([[1, 2]])
     labels = np.array([1])
     T = 2
     exp_res = (np.array([1, 2]), 1)
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.perceptron,
             exp_res, feature_matrix, labels, T):
-        return
+        return False
 
     feature_matrix = np.array([[1, 2], [-1, 0]])
     labels = np.array([1, 1])
     T = 2
     exp_res = (np.array([0, 2]), 2)
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.perceptron,
             exp_res, feature_matrix, labels, T):
-        return
+        return False
 
     log(green("PASS"), ex_name, "")
+    return True
 
 
-def check_average_perceptron():
-    ex_name = "Average perceptron"
+def test_average_perceptron_00():
+    ex_name = "Average perceptron 00"
 
     feature_matrix = np.array([[1, 2]])
     labels = np.array([1])
     T = 1
     exp_res = (np.array([1, 2]), 1)
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.average_perceptron,
             exp_res, feature_matrix, labels, T):
-        return
+        return False
 
     feature_matrix = np.array([[1, 2], [-1, 0]])
     labels = np.array([1, 1])
     T = 1
     exp_res = (np.array([-0.5, 1]), 1.5)
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.average_perceptron,
             exp_res, feature_matrix, labels, T):
-        return
+        return False
 
     feature_matrix = np.array([[1, 2]])
     labels = np.array([1])
     T = 2
     exp_res = (np.array([1, 2]), 1)
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.average_perceptron,
             exp_res, feature_matrix, labels, T):
-        return
+        return False
 
     feature_matrix = np.array([[1, 2], [-1, 0]])
     labels = np.array([1, 1])
     T = 2
     exp_res = (np.array([-0.25, 1.5]), 1.75)
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.average_perceptron,
             exp_res, feature_matrix, labels, T):
-        return
+        return False
 
     log(green("PASS"), ex_name, "")
+    return True
 
-
-def check_pegasos_single_update():
-    ex_name = "Pegasos single update"
+def test_pegasos_single_step_update_00():
+    ex_name = "Pegasos single step update 00"
 
     feature_vector = np.array([1, 2])
     label, theta, theta_0 = 1, np.array([-1, 1]), -1.5
     L = 0.2
     eta = 0.1
     exp_res = (np.array([-0.88, 1.18]), -1.4)
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.pegasos_single_step_update,
             exp_res,
             feature_vector, label, L, eta, theta, theta_0):
-        return
+        return False
 
     feature_vector = np.array([1, 1])
     label, theta, theta_0 = 1, np.array([-1, 1]), 1
     L = 0.2
     eta = 0.1
     exp_res = (np.array([-0.88, 1.08]), 1.1)
-    if check_tuple(
+    if not check_tuple(
             ex_name +  " (boundary case)", p1.pegasos_single_step_update,
             exp_res,
             feature_vector, label, L, eta, theta, theta_0):
-        return
+        return False
 
     feature_vector = np.array([1, 2])
     label, theta, theta_0 = 1, np.array([-1, 1]), -2
     L = 0.2
     eta = 0.1
     exp_res = (np.array([-0.88, 1.18]), -1.9)
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.pegasos_single_step_update,
             exp_res,
             feature_vector, label, L, eta, theta, theta_0):
-        return
+        return False
 
     log(green("PASS"), ex_name, "")
 
 
-def check_pegasos():
-    ex_name = "Pegasos"
+def test_pegasos_00():
+    ex_name = "Pegasos 00"
 
     feature_matrix = np.array([[1, 2]])
     labels = np.array([1])
     T = 1
     L = 0.2
     exp_res = (np.array([1, 2]), 1)
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.pegasos,
             exp_res, feature_matrix, labels, T, L):
-        return
+        return False
 
     feature_matrix = np.array([[1, 1], [1, 1]])
     labels = np.array([1, 1])
     T = 1
     L = 1
     exp_res = (np.array([1-1/np.sqrt(2), 1-1/np.sqrt(2)]), 1)
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.pegasos,
             exp_res, feature_matrix, labels, T, L):
-        return
+        return False
 
     log(green("PASS"), ex_name, "")
+    return True
 
-
-def check_classify():
-    ex_name = "Classify"
+def test_classify_00():
+    ex_name = "Classify 00"
 
     feature_matrix = np.array([[1, 1], [1, 1], [1, 1]])
     theta = np.array([1, 1])
     theta_0 = 0
     exp_res = np.array([1, 1, 1])
-    if check_array(
+    if not check_array(
             ex_name, p1.classify,
             exp_res, feature_matrix, theta, theta_0):
-        return
+        return False
 
     feature_matrix = np.array([[-1, 1]])
     theta = np.array([1, 1])
     theta_0 = 0
     exp_res = np.array([-1])
-    if check_array(
+    if not check_array(
             ex_name + " (boundary case)", p1.classify,
             exp_res, feature_matrix, theta, theta_0):
-        return
+        return False
 
     log(green("PASS"), ex_name, "")
+    return True
 
-def check_classifier_accuracy():
-    ex_name = "Classifier accuracy"
+def test_classifier_accuracy_00():
+    ex_name = "Classifier accuracy 00"
 
     train_feature_matrix = np.array([[1, 0], [1, -1], [2, 3]])
     val_feature_matrix = np.array([[1, 1], [2, -1]])
@@ -337,14 +367,14 @@ def check_classifier_accuracy():
     val_labels = np.array([-1, 1])
     exp_res = 1, 0
     T=1
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.classifier_accuracy,
             exp_res,
             p1.perceptron,
             train_feature_matrix, val_feature_matrix,
             train_labels, val_labels,
             T=T):
-        return
+        return False
 
     train_feature_matrix = np.array([[1, 0], [1, -1], [2, 3]])
     val_feature_matrix = np.array([[1, 1], [2, -1]])
@@ -353,19 +383,20 @@ def check_classifier_accuracy():
     exp_res = 1, 0
     T=1
     L=0.2
-    if check_tuple(
+    if not check_tuple(
             ex_name, p1.classifier_accuracy,
             exp_res,
             p1.pegasos,
             train_feature_matrix, val_feature_matrix,
             train_labels, val_labels,
             T=T, L=L):
-        return
+        return False
 
     log(green("PASS"), ex_name, "")
+    return True
 
-def check_bag_of_words():
-    ex_name = "Bag of words"
+def test_bag_of_words_00():
+    ex_name = "Bag of words 00"
 
     texts = [
         "He loves to walk on the beach",
@@ -375,18 +406,18 @@ def check_bag_of_words():
         res = p1.bag_of_words(texts)
     except NotImplementedError:
         log(red("FAIL"), ex_name, ": not implemented")
-        return
+        return False
+
     if not type(res) == dict:
         log(red("FAIL"), ex_name, ": does not return a tuple, type: ", type(res))
-        return
+        return False
 
     vals = sorted(res.values())
     exp_vals = list(range(len(res.keys())))
-    if not vals == exp_vals:
+    if vals != exp_vals:
         log(red("FAIL"), ex_name, ": wrong set of indices. Expected: ", exp_vals, " got ", vals)
-        return
+        return False
 
-    log(green("PASS"), ex_name, "")
 
     keys = sorted(res.keys())
     exp_keys = ['beach', 'better', 'he', 'is', 'loves', 'nothing', 'on', 'the', 'there', 'to', 'walk']
@@ -394,14 +425,17 @@ def check_bag_of_words():
 
     if keys == exp_keys:
         log(yellow("WARN"), ex_name, ": does not remove stopwords:", [k for k in keys if k not in stop_keys])
-    elif keys == stop_keys:
-        log(green("PASS"), ex_name, " stopwords removed")
-    else:
+        return False
+
+    if keys != stop_keys:
         log(red("FAIL"), ex_name, ": keys are missing:", [k for k in stop_keys if k not in keys], " or are not unexpected:", [k for k in keys if k not in stop_keys])
+        return False
 
+    log(green("PASS"), ex_name, " stopwords removed")
+    return True
 
-def check_extract_bow_feature_vectors():
-    ex_name = "Extract bow feature vectors"
+def test_extract_bow_feature_vectors_00():
+    ex_name = "Extract bow feature vectors 00"
     texts = [
         "He loves her ",
         "He really really loves her"]
@@ -441,18 +475,19 @@ def check_extract_bow_feature_vectors():
 def main():
     log(green("PASS"), "Import project1")
     try:
-        check_get_order()
-        check_hinge_loss_single()
-        check_hinge_loss_full()
-        check_perceptron_single_update()
-        check_perceptron()
-        check_average_perceptron()
-        check_pegasos_single_update()
-        check_pegasos()
-        check_classify()
-        check_classifier_accuracy()
-        check_bag_of_words()
-        check_extract_bow_feature_vectors()
+        test_get_order_00()
+        test_hinge_loss_single_00()
+        test_hinge_loss_single_01()
+        test_hinge_loss_full_00()
+        test_perceptron_single_update_00()
+        test_perceptron_00()
+        test_average_perceptron_00()
+        test_pegasos_single_step_update_00()
+        test_pegasos_00()
+        test_classify_00()
+        test_classifier_accuracy_00()
+        test_bag_of_words_00()
+        test_extract_bow_feature_vectors_00()
     except Exception:
         log_exit(traceback.format_exc())
 
