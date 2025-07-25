d084a8461f2cbb4108d7b6856b76119c67906d17
Robert Moser <rmoser@alum.mit.edu>
2025-07-25 10:52:11 -0400

    bulk commit before pull

--- AdventOfCode/2023/aoc23_12.py
+++ AdventOfCode/2023/aoc23_12.py
@@ -1,9 +1,18 @@
 # Advent of Code
+from itertools import groupby
+
 year = 2023
 day = 12
 
 import numpy as np
 import aocd
+import itertools as it
+import logging, sys
+import functools
+import math
+
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
+
 
 text0 = """
 ???.### 1,1,3
@@ -13,12 +22,61 @@ text0 = """
 ????.######..#####. 1,6,5
 ?###???????? 3,2,1
 """
+#
+# text0 = """
+# .##.?#??.#.?# 2,1,1,1
+# """
 text1 = aocd.get_data(day=day, year=year)
 
+
+@functools.cache
 def brute(record, score):
-    if sum(score) + len(score) - 1 == len(record):
-        return 1
-    return 99999
+    # if sum(score) + len(score) - 1 == len(record):
+    #     return 1
+
+    result = 0
+    if '.' in record:
+        records = [i for i in record.split('.') if i]
+        n_records = len(records)
+        n_scores = len(score)
+        for n in range(n_scores):
+            for i, group in enumerate(it.combinations_with_replacement(range(n_records), n_scores)):
+                print(records, list(group))
+                s = []
+                for j, id in enumerate(group):
+                    s.append(list())
+                    s[j].append(score[id] for )
+
+
+    return
+
+
+    # total = 1
+    count = 0
+    unknown = record.count('?')
+    broken = sum(score) - record.count('#')
+    unbroken = unknown - broken
+    # for group in it.permutations('#.', r=unknown):
+    logging.debug(f"Group:  {unknown}, {broken}, {unbroken}")
+    for group in it.product("#.", repeat=unknown):
+        l = list(group)
+        if not l.count('#') == broken:
+            continue
+        s = ''.join((c if c in '#.' else l.pop(0) for c in record))
+        logging.debug(f"{record}, {score}, {group}, {s}")
+        i = is_match(s, score)
+        if i:
+            # logging.debug(f"{record}, {score}, {group}, {s}")
+            pass
+        count += i
+
+    return count
+
+
+@functools.cache
+def is_match(record, score):
+    s = tuple(len(x) for x in record.split('.') if x)
+    return s == score
 
 
 if __name__ == '__main__':
@@ -27,27 +85,37 @@ if __name__ == '__main__':
 
     text = text0
     text = text.strip().splitlines()
+    text = text[1:2]
+
+    scores_1 = []
+    scores_2 = []
 
     for line in text:
+        # print(f"\n>>  {line}")
         record, score = line.split()
-        record = record.strip('.')
-        score = [int(i) for i in score.split(',')]
+        # record = record.strip('.')
+        score = tuple((int(i) for i in score.split(',')))
+        # logging.debug(f"{record} {score}")
 
-        while record[0] == '#':
-            record = record[score[0]+1:]
-            score.pop(0)
-            record = record.strip('.')
-            print(record, score)
+        # while len(record) and record[0] == '#':
+        #     i = score.pop(0)
+        #     record = record[i+1:]
+        #     record = record.strip('.')
+        #
+        # while len(record) and record[-1] == '#':
+        #     i = score.pop(-1)
+        #     record = record[:-i]
+        #     record = record.strip('.')
 
-        while record[-1] == '#':
-            record = record[:score[-1]]
-            score.pop(-1)
-            record = record.strip('.')
-            print(record, score)
 
+        scores_1.append(brute(record, score))
         break
+        record_2 = f"{record}?"*5
+        scores_2.append(brute(record_2, score*5))
 
-    print(record, score)
+    pone = sum(scores_1)
+    ptwo = sum(scores_2)
+    # print(record, score, record_count)
 
 
     print(f"AOC {year} day {day}  Part One: {pone}")
--- AdventOfCode/2023/aoc23_24.py
+++ AdventOfCode/2023/aoc23_24.py
@@ -22,7 +22,7 @@ if __name__ == '__main__':
     pone = ''
     ptwo = ''
 
-    text = text1
+    text = text0
     text = text.strip().splitlines()
     if text == text0:
         min_max = (7, 27)
@@ -37,27 +37,53 @@ if __name__ == '__main__':
         stones.append((x, y, z, dx, dy, dz))
 
     xy = np.zeros(shape=(len(stones) * (len(stones)-1) // 2, 4), dtype=float)
-    xyz = np.zeros(shape=(len(stones) * (len(stones)-1) // 2, 5), dtype=float)
+    xyz = np.zeros(shape=(len(stones) * (len(stones)-1) // 2, 6), dtype=float)
+    # xyz_a = np.zeros(shape=(len(stones) * (len(stones)-1) // 2, 4), dtype=float)
+    # xyz_b = np.zeros(shape=(len(stones), 1), dtype=float)
 
     for i, (a, b) in enumerate(it.combinations(stones, 2)):
         # print(i, a, b)
         xy_a = np.array([[a[4], -a[3], 0, 0], [b[4], -b[3], 0, 0], [1, 0, -a[3], 0], [0, 1, 0, -b[4]]])
         xy_b = np.array([[a[0] * a[4] - a[1] * a[3]], [b[0]*b[4]-b[1]*b[3]], [a[0]], [b[1]]])
 
-        # xy_a = np.array([[1, 0, 0, 0, -a[0], 0],
-        #                  [0, 1, 0, 0, -a[1], 0],
-        #                  [0, 0, 1, 0, 0, -b[0]],
-        #                  [0, 0, 0, 1, 0, -b[1]],
-        #                  [1, 0, -1, 0, 0, 0],
-        #                  [0, 1, 0, -1, 0, 0]
-        #                 ])
-        # xy_b = np.array([[a[3], a[4], b[3], b[4], 0, 0]]).T
         try:
             xy[i] = sp.linalg.solve(xy_a, xy_b).T
         except np.linalg.LinAlgError:
             xy[i] = np.repeat(np.inf, xy.shape[1])
 
     pone = np.bitwise_and(((min_max[0], min_max[0], 0, 0) <= xy), (xy <= (min_max[1], min_max[1], np.inf, np.inf))).all(axis=1).sum()
+
+
+    A = [[], [], []]
+    B = [[], [], []]
+
+    z = list(np.zeros(shape=len(stones)))
+    for i, s in enumerate(stones):
+        _a = [1] + z.copy()
+        _a[i+1] = -s[3]
+        A[0].append(_a)
+        B[0].append(s[0])
+
+        _a = [1] + z.copy()
+        _a[i+1] = -s[4]
+        A[1].append(_a)
+        B[1].append(s[1])
+
+        _a = [1] + z.copy()
+        _a[i+1] = -s[5]
+        A[2].append(_a)
+        B[2].append(s[2])
+
+    A[0].append([0] + z.copy())
+    A[1].append([0] + z.copy())
+    A[2].append([0] + z.copy())
+    B[0].append(0)
+    B[1].append(0)
+    B[2].append(0)
+
+    A = np.array(A)
+    B = np.array(B)
+    sp.linalg.solve(A[0], B[0])
 
     print(f"AOC {year} day {day}  Part One: {pone}")
 
