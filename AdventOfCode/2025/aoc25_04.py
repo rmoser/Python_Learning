ef1c7d2e507bb1f078d153bc409570dbb59cea2d
rmoser2000 <rmoser@alum.mit.edu>
2025-12-04 16:44:31 -0500

    AOCD 2025 Day 4 complete

--- AdventOfCode/2025/aoc25_04.py
+++ AdventOfCode/2025/aoc25_04.py
@@ -27,12 +27,24 @@ if __name__ == '__main__':
     pone = ''
     ptwo = ''
 
-    text = text0
+    text = text1
     text = text.strip().splitlines()
 
     arr = (np.array([[c for c in line] for line in text]) == '@').astype(int)
-    arr_neighbors = None
+
+    sums = utils.sum_neighbors(arr)
+    pone = (sums[np.where(arr)] < 4).sum()
 
     print(f"AOC {year} day {day}  Part One: {pone}")
+
+
+    _arr = arr.copy()
+
+    while (sums[np.where(_arr)] < 4).any():
+        _arr[np.where(sums<4)] = 0
+        sums = utils.sum_neighbors(_arr)
+        # ic(_arr, sums)
+
+    ptwo = arr.sum() - _arr.sum()
 
     print(f"AOC {year} day {day}  Part Two: {ptwo}")
