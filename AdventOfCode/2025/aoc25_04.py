aa11cf71bdc811d5c4d712cb393047b397bf0d6b
Robert Moser <rmoser@alum.mit.edu>
2025-12-04 07:57:16 -0500

    AOC 2025 day 4 stub

---
+++ AdventOfCode/2025/aoc25_04.py
@@ -0,0 +1,38 @@
+# Advent of Code
+year = 2025
+day = 4
+
+import numpy as np
+import aocd
+import os
+import utils
+from pprint import pprint
+from icecream import ic
+
+text0 = """
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+"""
+text1 = aocd.get_data(day=day, year=year)
+
+if __name__ == '__main__':
+    pone = ''
+    ptwo = ''
+
+    text = text0
+    text = text.strip().splitlines()
+
+    arr = (np.array([[c for c in line] for line in text]) == '@').astype(int)
+    arr_neighbors = None
+
+    print(f"AOC {year} day {day}  Part One: {pone}")
+
+    print(f"AOC {year} day {day}  Part Two: {ptwo}")
