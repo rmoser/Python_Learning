a37a907cb71dddc029818d4860f4f16b50f26b75
Robert Moser <rmoser@alum.mit.edu>
2025-12-11 11:26:15 -0500

    AOC 2025 Day 11 complete

--- AdventOfCode/2025/aoc25_11.py
+++ AdventOfCode/2025/aoc25_11.py
@@ -23,17 +23,33 @@ ggg: out
 hhh: ccc fff iii
 iii: out
 """
+
+text0 = """svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out"""
+
 text1 = aocd.get_data(day=day, year=year)
 
 Network = dict()
 
 @functools.cache
-def paths_to_out(node):
+def paths_to(node, end='out'):
     if node not in Network:
         return 0
-    if 'out' in Network[node]:
+    if end in Network[node]:
         return 1
-    return sum(paths_to_out(n) for n in Network[node])
+    return sum(paths_to(n, end) for n in Network[node])
+
 
 if __name__ == '__main__':
     pone = ''
@@ -47,9 +63,14 @@ if __name__ == '__main__':
         _in, _out = line.split(': ')
         _out = set(_out.strip().split())
         network[_in] = _out
-    ic(network)
 
-    pone = paths_to_out('you')
+    pone = paths_to('you')
     print(f"AOC {year} day {day}  Part One: {pone}")
+
+    fft_dac = paths_to('fft', 'dac')
+    if fft_dac == 0:
+        ptwo = paths_to('svr', 'dac') * paths_to('dac', 'fft') * paths_to('fft', 'out')
+    else:
+        ptwo = paths_to('svr', 'fft') * fft_dac * paths_to('dac', 'out')
 
     print(f"AOC {year} day {day}  Part Two: {ptwo}")
